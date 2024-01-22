6fe3ba18a928d1da9c0768b1620dc217348b9ed5
strokerox <148830055+strokerox@users.noreply.github.com>
2024-01-22 17:40:25 -0400

    Update deportes.cpp

--- deportes.cpp
+++ deportes.cpp
@@ -1,1 +1,20 @@
-#include 
+#include <stdio.h>
+#include <string>
+
+using namespace std;
+
+struct dato{
+  char nombre[40];
+  char pais[25];
+};
+struct atleta{
+  char deporte[30];
+  struct datos pers;
+  int nro_medallas;
+};
+
+void encabezado(){
+    printf("*** ___________M E D A L L A S  D E L  A T L E T A___________ ***");
+    printf("\n");
+    printf("\n");
+}
