6df86b6d0849635db4ab114484f400a4faa1fd1d
strokerox <148830055+strokerox@users.noreply.github.com>
2024-01-23 00:11:43 -0400

    Update deportes.cpp

--- deportes.cpp
+++ deportes.cpp
@@ -17,4 +17,18 @@ void encabezado(){
     printf("*** ___________M E D A L L A S  D E L  A T L E T A___________ ***");
     printf("\n");
     printf("\n");
-}
+};
+
+int main(){
+    struct dato ats [10];
+    int pos, P_mayor, P_menor;
+    int M_mayor, M_menor;
+        M_mayor = 0;
+        M_menor = 50;
+    struct datos ats[] = {
+        {"Nombre1", "Pais1", "Deporte1", 5},
+        {"Nombre2", "Pais2", "Deporte2", 3},
+        {"Nombre3", "Pais3", "Deporte3", 7}
+        // otros atletas
+    };
+};
